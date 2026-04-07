fbcd0b23a1f28ada03241785be501a005fd99ee2
Álvaro Díaz <varodv@gmail.com>
2026-04-07 22:40:34 +0200

    build: Add @tanstack/react-query-devtools

--- providers/query-client-provider.tsx
+++ providers/query-client-provider.tsx
@@ -5,9 +5,15 @@ import {
   QueryClient,
   QueryClientProvider as TanstackQueryClientProvider,
 } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 export function QueryClientProvider({ children }: PropsWithChildren) {
-  return <TanstackQueryClientProvider client={queryClient}>{children}</TanstackQueryClientProvider>;
+  return (
+    <TanstackQueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </TanstackQueryClientProvider>
+  );
 }
